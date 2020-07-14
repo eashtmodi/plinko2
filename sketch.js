46ba9e7c88a2a54fc63ab04275fd4272c6da5eea
eashtmodi <eashtmodi1006@gmail.com>
2020-07-14 18:47:38 +0530

    plinky

--- sketch.js
+++ sketch.js
@@ -11,6 +11,9 @@ var divisions=[];
 
 var divisionHeight=300;
 
+var score=0;
+var turns=5;
+
 function setup(){
     var canvas = createCanvas(480,800);
     engine = Engine.create();
@@ -24,6 +27,7 @@ function draw(){
     Engine.update(engine);
     ground.display();
 
+  
     for(var a=0;a<=width;a=a+80){
       divisions.push(new Division(a,height-divisionHeight/2,10,160));
       
@@ -37,10 +41,6 @@ function draw(){
       plinkos.push(new Plinko(c,175,5));
     }
 
-     if(frameCount%30===0){
-       particles.push(new Particle(240,-40,5));  
-     }   
-
      for(var d=0;d<divisions.length;d=d+80){
      divisions[d].display();
      }
@@ -53,5 +53,33 @@ function draw(){
       particles[e].display();
       }
 
+      console.log(turns);
+
+      push();
+      textSize(18);
+      text("500",20,700);
+      text("400",100,700);
+      text("300",180,700);
+      text("300",260,700);
+      text("400",340,700);
+      text("500",420,700);
+      pop();
+      
+      textSize(48);
+      text("SCORE="+score,100,300);
+      text("TURNS="+turns,100,400);
+
+      if(turns===0){
+        textSize(48);
+        text("GAME_OVER",70,500);
+      }
+}
+
+function mousePressed(){
+  if(turns>0){
+    particles.push(new Particle(mouseX,mouseY,5)); 
+    turns--; 
+    score=score+500;
+  }
 }
 
